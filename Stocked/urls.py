ca9a9101d8a5077a07ba88990f5bff23893f43e7
unknown <akfaqih3@gmail.com>
2024-05-22 15:16:15 -0700

    finish design Stocked

--- Stocked/urls.py
+++ Stocked/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='Stocked'),
-    path('Stores/',views.Stores,name='Stores'),
-    path('Brands/',views.Brands,name='Brands'),
-    path('Categories/',views.Categories,name='Categories'),
-    path('Units/',views.Units,name='Units'),
-    path('Products/',views.Products,name='Products')
+    path('<template_name>/',views.Stocked,name='Stocked')
 ]
--- Stocked/views.py
+++ Stocked/views.py
@@ -1,38 +1,11 @@
-from django.shortcuts import render
-from pathlib import Path
+
+from django.shortcuts import render 
 # Create your views here.
 
-def index(request):
-    Context ={'PageName':'','Page':'includes/index.html',
-              'Breadcrumbs':{'Home':'Home.html','Stock':'Stock.html'}}
-              
-    return render(request,'index.html',Context)
 
-def Stores(request):
-    Context ={'PageName':'Stores','Page':'includes/Stores.html','New':'#NewStoreModal',
-              'Breadcrumbs':{'Home':'Home.html','Stock':'Stock.html','Stores':'Stores.html'}}
-              
-    return render(request,'index.html',Context)
-
-def Brands(request):
-    Context ={'PageName':'Brands','Page':'includes/Brands.html','New':'#NewBrandModal',
-              'Breadcrumbs':{'Home':'Home.html','Stock':'Stock.html','Brands':'Brands.html'}}
-              
-    return render(request,'index.html',Context)
-def Categories(request):
-    Context ={'PageName':'Categories','Page':'includes/Categories.html','New':'#NewCategoryModal',
-              'Breadcrumbs':{'Home':'Home.html','Stock':'Stock.html','Categories':'Categories.html'}}
-              
-    return render(request,'index.html',Context)
-
-def Units(request):
-    Context ={'PageName':'Units','Page':'includes/Units.html','New':'#NewUnitModal',
-              'Breadcrumbs':{'Home':'Home.html','Stock':'Stock.html','Units':'Units.html'}}
-              
-    return render(request,'index.html',Context)
-
-def Products(request):
-    Context ={'PageName':'Products','Page':'includes/Products.html','New':'#NewProductModal',
-              'Breadcrumbs':{'Home':'Home.html','Stock':'Stock.html','Products':'Products.html'}}
+def Stocked(request,template_name):
+    Context ={'PageName':template_name,'template':'includes/'+template_name+'.html','New':'#New'+template_name+'Modal',
+              'Breadcrumbs':['Stock',template_name]}
     
-    return render(request,'index.html',Context)
+
+    return render(request,'Stocked.html',Context)
